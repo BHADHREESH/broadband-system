203625e7b2f0a2f7cb659a7e37c04fb30810eadf
Bhadhreesh <bhadhreesh2006@gmail.com>
2026-06-02 21:34:05 +0530

    Add GitHub Pages static entry

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require("./server");
+(() => {
+    const year = new Date().getFullYear();
+    const copyright = document.getElementById("copyrightText");
+
+    if (copyright) {
+        copyright.textContent = `(c) ${year} NetWave Broadband. All rights reserved.`;
+    }
+})();
